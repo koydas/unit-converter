3d5cf5a677da17e8e8ec849cf978083eb1777b26
Stephane Hamel <koydas@hotmail.com>
2024-10-15 12:04:02 -0400

    redeploy

--- frontend/unit-converter/next.config.mjs
+++ frontend/unit-converter/next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: "https://github.com/koydas/unit-converter/tree/main/docs/",
   output: 'export',
   images: {
     unoptimized: true
